6778491970c8a650909e3591a60d54a4d4b5791f
Maximiliano Genolet <maximilianogenolet@gmail.com>
2022-10-14 11:05:11 -0300

    Se modifico app.js y creo estructura base index.html

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 
-const app = express ();
+const app = express();
+const path = require("path");
 
-const path = require ('path')
+app.use("/static", express.static("public"));
 
-app.use ('/static', express.static('public'));
+app.listen(3030, () => {
+  console.log("Servidor funcionando");
+});
 
-app.listen(3030, () =>{
-    console.log('Servidor funcionando')
-})
-
-app.get('/', (req, res) => {
-    res.sendFile (path.resolve('./views/home.html'));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./views/index.html"));
+});
