eadc285ca12aa0be81300cf295c59a2c80f8e66d
Maximiliano Genolet <maximilianogenolet@gmail.com>
2022-11-09 11:44:51 -0300

    motor de plantilla ok , mas controladores de index y detalle

--- app.js
+++ app.js
@@ -3,32 +3,39 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+//configuro lo static para express
 app.use("/static", express.static("public"));
+
+//Configuro motor de plantilla
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/src/views"));
+
+const indexRouter = require("./src/routes/index");
+const usersRouter = require("./src/routes/users");
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 app.listen(3030, () => {
   console.log("Servidor funcionando");
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve("./views/index.html"));
-});
-
-app.get("/register", (req, res) => {
+/* app.get("/register", (req, res) => {
   res.sendFile(path.resolve("./views/register.html"));
-});
+}); */
 
-app.get("/login", (req, res) => {
+/* app.get("/login", (req, res) => {
   res.sendFile(path.resolve("./views/login.html"));
-});
+}); */
 
-app.get("/productDetail", (req, res) => {
+/* app.get("/productDetail", (req, res) => {
   res.sendFile(path.resolve("./views/productDetail.html"));
-});
+}); */
 
-app.get("/productIndex", (req, res) => {
+/* app.get("/productIndex", (req, res) => {
   res.sendFile(path.resolve("./views/productIndex.html"));
-});
+}); */
 
-app.get('/productCart', (req, res) => {
-  res.sendFile(path.resolve('./views/productCart.html'))
-});
+/* app.get("/productCart", (req, res) => {
+  res.sendFile(path.resolve("./views/productCart.html"));
+}); */
