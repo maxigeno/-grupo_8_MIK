53a19354d0e5f22a51c03b552a2019d488d09d03
Maximiliano Genolet <maximilianogenolet@gmail.com>
2022-11-29 12:35:44 -0300

    Se corregio y termino el CRUD

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -10,64 +10,93 @@ const calculateDiscount = (price, discountPer) => {
 };
 
 const productsControllers = {
-  index: (req, res) => {
+  // Root - Show all products
+  products: (req, res) => {
     let newCourse = products.filter((product) => product.isNew == true);
+    console.log(newCourse, newCourse);
     let inSale = products.filter((product) => product.inSale == true);
-    let others = products.filter((product) => product.isNew == false && product.inSale == false)
+    let others = products.filter(
+      (product) => product.isNew == false && product.inSale == false
+    );
     res.render("index", { newCourse, inSale, others, calculateDiscount });
   },
+  // Detail
   detail: (req, res) => {
-    let id = (req.params.id);
-    let product = products.find(product => product.id === id);
+    let id = req.params.id;
+    let product = products.find((product) => product.id == id);
     res.render("productDetail", { product });
   },
+  // Create Form
   create: (req, res) => {
     res.render("productCreate");
   },
+  // Create Method POST
   store: (req, res) => {
-    console.log("body", req.body);
+    let image;
+    if (req.file != undefined) {
+      image = req.file.filename;
+    } else {
+      image = "default-image.png";
+    }
+
     let newProduct = {
       id: products[products.length - 1].id + 1,
       ...req.body,
-      image: "default-img.png"
+      isNew: req.body.isNew === "true" ? true : false,
+      inSale: req.body.inSale === "true" ? true : false,
+      image,
     };
-
-    console.log("json", req.body)
-    products.push(newProduct)
-    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '));
-    res.redirect('/')
+    products.push(newProduct);
+    //console.log("products", products);
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
+    res.redirect("/");
   },
+  // Update  Form to edit
   edit: (req, res) => {
-    let id = req.params.id
-    let productToEdit = products.find(product => product.id == id)
-    res.render('productEdit', {productToEdit})
+    let id = req.params.id;
+    let productToEdit = products.find((product) => product.id == id);
+    res.render("productEdit", { productToEdit });
   },
+  // Update - Method to update
   update: (req, res) => {
     let id = req.params.id;
-    let productToEdit = products.find(product => product.id == id)
+    let productToEdit = products.find((product) => product.id == id);
+    let image;
+
+    if (req.file != undefined) {
+      image = req.file.filename;
+    } else {
+      image = "default-image.png";
+    }
 
     productToEdit = {
       id: productToEdit.id,
       ...req.body,
-      image: productToEdit.image,
+      isNew: req.body.isNew === "true" ? true : false,
+      inSale: req.body.inSale === "true" ? true : false,
+      image,
     };
 
-    let newProducts = products.map(product =>{
-      if (product.id == productToEdit.id){
-        return product = {...productToEdit}
+    let newProducts = products.map((product) => {
+      if (product.id == productToEdit.id) {
+        return (product = { ...productToEdit });
       }
       return product;
-    })
+    });
 
-    fs.writeFileSync(productsFilePath, JSON.stringify(newProducts, null , " "))
-    res.redirect('/')
+    fs.writeFileSync(productsFilePath, JSON.stringify(newProducts, null, " "));
+    res.redirect("/");
   },
+
   destroy: (req, res) => {
     let id = req.params.id;
-    let finalProducts = products.filter (product => product.id != id);
-    fs.writeFileSync(productsFilePath, JSON.stringify(finalProducts, null, " "));
-    res.redirect('/');
-  }
+    let finalProducts = products.filter((product) => product.id != id);
+    fs.writeFileSync(
+      productsFilePath,
+      JSON.stringify(finalProducts, null, " ")
+    );
+    res.redirect("/");
+  },
 };
 
 module.exports = productsControllers;
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,16 +1,41 @@
 const express = require("express");
 const router = express.Router();
-const multer = require('multer');
+const multer = require("multer");
+const path = require("path");
 
+// ************ Requiero el controlador ************
 const productsController = require("../controllers/productsController");
 
-router.get("/index", productsController.index);
-router.get("/create", productsController.create);
-router.post("/", productsController.store);
-router.get("/edit/:id", productsController.edit);
-router.patch("/edit/:id", productsController.update);
-router.delete("/delete/:id", productsController.destroy);
+// ************ multer ************
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, path.join(__dirname, "../../public/img"));
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
 
+const upload = multer({ storage });
+
+//*********GET PRODUCTS INDEX*********** */
+router.get("/", productsController.products);
+
+/*** CREATE ONE PRODUCT ***/
+router.get("/create", productsController.create);
+router.post("/", upload.single("image"), productsController.store);
+
+/*** GET ONE PRODUCT ***/
 router.get("/:id/", productsController.detail);
+
+/*** EDIT ONE PRODUCT ***/
+router.get("/edit/:id", productsController.edit);
+router.patch("/edit/:id", upload.single("image"), productsController.update);
+
+/*** DELETE ONE PRODUCT***/
+router.delete("/delete/:id", productsController.destroy);
 
 module.exports = router;
