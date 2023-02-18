249659bbfdc87dd4500011fb71576fa1395f3f53
1vanSoria <o.ivansoria@gmail.com>
2023-02-18 17:08:55 -0300

    base de datos poblada

--- db/mik_db.sql
+++ db/mik_db.sql
@@ -62,7 +62,14 @@ INSERT INTO `categorias` (`nombre`) VALUES
 
 /* insert  productos */
 INSERT INTO `productos` ( `nombre`, `descripcion`, `descripcion_corta`, `precio`, `imagen`, `nuevo`, `destacado`, `porcentaje_descuento`, `categoria_id`) VALUES
-( 'curso de yoga', 'ayudara a los niños apoder encontrar...', 'curso de yoga para miños' , 4500.00, 'camisa.jpg', 1, 0,  10, 1);
+( 'curso de yoga', 'ayudara a los niños apoder encontrar...', 'curso de yoga para miños' , 4500.00, 'camisa.jpg', 1, 0,  10, 1),
+( 'Curso de Natación', '¿Quieres que tu hijo aprenda a nadar? En MIK acompañamos este proceso por medio de los diferentes niveles que te ofrecemos en este curso donde tu hijo podrá familiarizarse con el agua, adquiriendo habilidades básicas que se perfeccionaran a medida que supere cada nivel y con las que finalmente logrará realizar movimientos técnicos de natación.', '' , 9970, 'natacion.webp', 1, 0,  0, 1),
+( 'Curso de Voleibol', '¿Estas interesado en mejorar tus capacidades físicas? En este curso tendras la oportunidad de ejercitar piernas y gluteos, conservando el tono musular y mejorar la coordinación, fuerza, resistencia, flexibilidad, equilibrio, además encontrarás un espacio amigable para que mejores en este deporte cada dia de la mano de nuestros profesionales que te acompañaran en cada parte del proceso. Este curso va dirigido para jovenes de 11 años en adelante y cuenta con cuatro sesiones de 1 hora y media cada una.', '' , 6770, 'voleibal.webp', 0, 1,  40, 1),
+( 'Curso de reposteria en familia', '¡Llegó el plan familiar que estabas buscando! Conviertete junto con un familia en chef por dos horas cada semana y diviértete preparando un postre delicioso con diferentes tecnicas para que disfruten un momento increible en familia. Esta clase cuenta con 3 sesiones de 2 horas cada una y esta dirigida a todo el grupo familiar que quiera venir y divertirse.', '' , 10770, 'cocina.webp', 0, 1,  30, 4),
+( 'Curso de Guitarra', 'Si te gusta tocar guitarra, compra nuestro curso e inicia tu aprendizaje en este instrumento musical. Aprenderás técnicas básicas como esclas, acorders, ritmos, gramática musical, entre otros para que logres interpretar tus primeras canciones. ¡Incríbete ahora!', '' , 14000, 'guitarra.webp', 0, 1,  20, 2),
+( 'Futbol para niños', '¿Te gusta jugar futbol? Mejora tus habilidades y aprende sobre las tecnicas en este deporte inscribiendote a nuestro curso de futbol dirigido a niños con el objetivo de acompañarte en todo el proceso de crecimiento dentro de este deporte. Aprenderas diferentes tecnicas y mejoraras tus habilidades de la mano de nuestros profesionales.', '' , 6800, 'futbil.webp', 0, 1,  50, 1);
+
+
 
 /* insert usuario */
 INSERT INTO `usuarios` ( `nombre`, `apellido`, `email`, `password`, `rol`) VALUES
